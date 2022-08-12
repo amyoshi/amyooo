427479d345601e4af287b1155056b672ae197046
amyoshi <amyonwoo@gmail.com>
2022-08-12 18:21:48 -0400

    making progress, general additions to site

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,7 @@
 /* function calls menu element, gives menu the class open and toggles it */
+let logo = document.getElementbyId("amyo");
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+/* when hamburger menu is open, website logo so dissapear. when hamburger menu is closed, logo should be visable */
