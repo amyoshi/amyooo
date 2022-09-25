eef6dc08891e806f8479a6df8b437f8f7697b957
amyoshi <amyonwoo@gmail.com>
2022-09-24 22:15:10 -0400

    lots of new things site is looking goodish

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,29 @@
+/* when hamburger menu is open, website logo so dissapear. when hamburger menu is closed, logo should be visable */
+let loader = document.getElementById("preloader");
+let loaderloader = document.getElementById("preloader-bg");
+
+let dismissLoadingImg = function () {
+  loader.style.display = "none";
+};
+
+let dismissLoadingScreen = function () {
+  loaderloader.style.display = "none";
+};
+
+let wait3second = function () {
+  let result = setTimeout(dismissLoadingScreen, 1800);
+};
+
+let wait3seconds = function () {
+  let result = setTimeout(dismissLoadingImg, 1800);
+};
+
+window.addEventListener("load", wait3second);
+window.addEventListener("load", wait3seconds);
+
 /* function calls menu element, gives menu the class open and toggles it */
 let logo = document.getElementbyId("amyo");
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
-/* when hamburger menu is open, website logo so dissapear. when hamburger menu is closed, logo should be visable */
